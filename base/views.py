202a3fb7d00fe6410765dc3c124881cfc7facfcf
Amit K Rao <amitraok@gmail.com>
2016-02-21 23:34:40 +0000

    Added routes and table details template page

--- base/views.py
+++ base/views.py
@@ -9,13 +9,20 @@ def page_not_found_handler(error):
 @app.route('/')
 def index():
     conn = sqlite3.connect('appdata.db')
-    cur = conn.execute("SELECT type, name, tbl_name, sql FROM sqlite_master WHERE type='table'")
+    cur = conn.execute("SELECT type, tbl_name FROM sqlite_master WHERE type='table'")
     query_results = cur.fetchall()
     cur.close()
     conn.close()
     
-    return render_template('index.html', tables = query_results)
+    return render_template('index.html', db_objects=query_results)
 
-@app.route('/dashboard')
-def show_dashboard():
-    return render_template('dashboard.html')
+@app.route('/details/<object_name>')
+def show_object(object_name):
+    conn = sqlite3.connect('appdata.db')
+    sql_query = "pragma table_info(%s)"%object_name
+    cur = conn.execute(sql_query)
+    query_results = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    return render_template('details.html', object_name=object_name, query_results=query_results)
